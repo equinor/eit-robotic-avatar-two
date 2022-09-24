9079f2db23dddf260909d5e54627f8aac03a122a
Ole Martin Gjersvik <olgj@equinor.com>
2022-09-24 10:49:54 +0000

    yew hello world

--- .devcontainer/cargo-tools.sh
+++ .devcontainer/cargo-tools.sh
@@ -4,5 +4,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 cd ~
+
+# Tooling for webasembely deveopment.
+rustup target add wasm32-unknown-unknown
 wget -qO- https://github.com/thedodd/trunk/releases/download/v0.16.0/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
 mv ./trunk /usr/local/bin/
