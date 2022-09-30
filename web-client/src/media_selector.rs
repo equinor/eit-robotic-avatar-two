906322b1abbd0b45c78d25b357866a4651c62707
Ole Martin Gjersvik <olgj@equinor.com>
2022-09-30 13:22:50 +0000

    Device list ui.

--- web-client/src/media_selector.rs
+++ web-client/src/media_selector.rs
@@ -1,17 +1,18 @@
-use web_sys::{MediaStream, MediaDeviceInfo};
-use weblog::console_log;
+use web_sys::{MediaDeviceInfo, MediaDeviceKind, MediaStream};
 use yew::{html, Component, Context, Html, Properties};
 
-use crate::js::media::{get_user_video, enumerate_devices_callback};
+use crate::js::media::{enumerate_devices_callback, get_user_video};
 
 #[derive(PartialEq, Eq, Properties)]
 pub struct Props;
 
-pub struct MediaSelector {}
+pub struct MediaSelector {
+    device_list: Vec<MediaDeviceInfo>,
+}
 
 pub enum Msg {
     LeftVideo(MediaStream),
-    DeviceInfo(Vec<MediaDeviceInfo>)
+    DeviceInfo(Vec<MediaDeviceInfo>),
 }
 
 impl Component for MediaSelector {
@@ -20,27 +21,61 @@ impl Component for MediaSelector {
 
     fn create(ctx: &Context<Self>) -> Self {
         get_user_video(ctx.link().callback(Msg::LeftVideo));
-        MediaSelector {}
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            "Media Selector"
+        MediaSelector {
+            device_list: Vec::new(),
         }
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::LeftVideo(video) => {
-                console_log!(video);
+            Msg::LeftVideo(_video) => {
                 enumerate_devices_callback(ctx.link().callback(Msg::DeviceInfo));
-            },
+            }
             Msg::DeviceInfo(list) => {
-                for device in list.iter() {
-                    console_log!(device);
-                }
+                self.device_list = list;
             }
         }
-        false
+        true
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        fn kind_name(kind: MediaDeviceKind) -> &'static str {
+            match kind {
+                MediaDeviceKind::Audioinput => "Microphone",
+                MediaDeviceKind::Audiooutput => "Speaker",
+                MediaDeviceKind::Videoinput => "Camera",
+                _ => "Unknown",
+            }
+        }
+
+        let devices = self.device_list.iter().map(|info| {
+            html!(
+                <tr>
+                    <td>{kind_name(info.kind())}</td>
+                    <td>{info.label()}</td>
+                </tr>
+            )
+        });
+
+        html! {
+            <div>
+                <h2> {"Media Selector"}</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>{"Type"}</th>
+                            <th>{"Name"}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td>{"None"}</td>
+                        </tr>
+                        {for devices}
+                    </tbody>
+                </table>
+            </div>
+        }
     }
 }
