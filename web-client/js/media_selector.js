39c804f78e5c5c59e8b3be8a3fda66a09d1755fb
Ole Martin Gjersvik <olgj@equinor.com>
2022-10-01 07:05:06 +0200

    Ask for device list (#19)
    * Get device list from browser
    * Device list ui.

--- web-client/js/media_selector.js
+++
@@ -1,3 +0,0 @@
-export async function user_video_id(id) {
-    return await navigator.mediaDevices.getUserMedia({video: true});
-}
--- web-client/src/media_selector.rs
+++ web-client/src/media_selector.rs
@@ -1,16 +1,19 @@
-use web_sys::MediaStream;
-use weblog::console_log;
+use web_sys::{MediaDeviceInfo, MediaDeviceKind, MediaStream};
 use yew::{html, Component, Context, Html, Properties};
 
-use crate::js::media::get_user_video;
+use crate::media::MediaService;
 
 #[derive(PartialEq, Eq, Properties)]
 pub struct Props;
 
-pub struct MediaSelector {}
+pub struct MediaSelector {
+    service: MediaService,
+    device_list: Vec<MediaDeviceInfo>,
+}
 
 pub enum Msg {
     LeftVideo(MediaStream),
+    DeviceInfo(Vec<MediaDeviceInfo>),
 }
 
 impl Component for MediaSelector {
@@ -18,20 +21,66 @@ impl Component for MediaSelector {
     type Properties = Props;
 
     fn create(ctx: &Context<Self>) -> Self {
-        get_user_video(ctx.link().callback(Msg::LeftVideo));
-        MediaSelector {}
+        let service = MediaService::new();
+
+        service.get_user_media_callback(ctx.link().callback(Msg::LeftVideo));
+        MediaSelector {
+            service,
+            device_list: Vec::new(),
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::LeftVideo(_video) => {
+                self.service
+                    .enumerate_devices_callback(ctx.link().callback(Msg::DeviceInfo));
+            }
+            Msg::DeviceInfo(list) => {
+                self.device_list = list;
+            }
+        }
+        true
     }
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            "Media Selector"
+        fn kind_name(kind: MediaDeviceKind) -> &'static str {
+            match kind {
+                MediaDeviceKind::Audioinput => "Microphone",
+                MediaDeviceKind::Audiooutput => "Speaker",
+                MediaDeviceKind::Videoinput => "Camera",
+                _ => "Unknown",
+            }
         }
-    }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::LeftVideo(video) => console_log!(video),
+        let devices = self.device_list.iter().map(|info| {
+            html!(
+                <tr>
+                    <td>{kind_name(info.kind())}</td>
+                    <td>{info.label()}</td>
+                </tr>
+            )
+        });
+
+        html! {
+            <div>
+                <h2> {"Media Selector"}</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>{"Type"}</th>
+                            <th>{"Name"}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td>{"None"}</td>
+                        </tr>
+                        {for devices}
+                    </tbody>
+                </table>
+            </div>
         }
-        false
     }
 }
