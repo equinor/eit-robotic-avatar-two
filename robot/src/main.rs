f1783b54be94a02f29a8d5bfe080f77dcfc53ccf
Ole Martin Gjersvik <olgj@equinor.com>
2022-10-01 07:52:53 +0000

    Robot can send message to server

--- robot/src/main.rs
+++ robot/src/main.rs
@@ -1,6 +1,15 @@
+use common::SendMessage;
+
 #[tokio::main]
 async fn main() {
     let server = robot::setup().await;
 
     println!("Config from server: {:?}", server.config());
+
+    let msg = SendMessage {
+        topic: "robot".to_string(),
+        msg_type: "hello".to_string(),
+        payload: Vec::new(),
+    };
+    server.send_message(msg).await.unwrap();
 }
--- robot/src/server.rs
+++ robot/src/server.rs
@@ -1,29 +1,46 @@
 use anyhow::Result;
-use common::{RobotConfig, RobotRegister};
-use reqwest::Client;
+use common::{RobotConfig, RobotRegister, SendMessage};
+use reqwest::{Client, Url};
 
 use crate::config::LocalConfig;
 
 pub struct Server {
+    base_url: Url,
+    client: Client,
     config: RobotConfig,
 }
 
 impl Server {
     pub async fn connect(config: LocalConfig, register: RobotRegister) -> Result<Server> {
         let client = Client::new();
+        let base_url = config.base_url;
 
         let config = client
-            .post(config.base_url.join("api/robot/register")?)
+            .post(base_url.join("api/robot/register")?)
             .json(&register)
             .send()
             .await?
             .json()
             .await?;
 
-        Ok(Server { config })
+        Ok(Server {
+            base_url,
+            client,
+            config,
+        })
     }
 
     pub fn config(&self) -> &RobotConfig {
         &self.config
+    }
+
+    pub async fn send_message(&self, msg: SendMessage) -> Result<()> {
+        self.client
+            .post(self.base_url.join("api/messaging")?)
+            .json(&msg)
+            .send()
+            .await?;
+
+        Ok(())
     }
 }
