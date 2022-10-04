9479698591dcdf7e519bed168096cc9b831ec840
Ole Martin Gjersvik <olgj@equinor.com>
2022-10-04 12:30:32 +0000

    Auth middleware warning

---
+++ server/src/auth.rs
@@ -0,0 +1,62 @@
+use axum::{
+    http::{self, Request, StatusCode},
+    middleware::Next,
+    response::Response,
+};
+use hmac::Hmac;
+use jwt::VerifyWithKey;
+use log::warn;
+use sha2::Sha256;
+
+use crate::Config;
+
+#[derive(Debug, Clone)]
+pub struct Auth {
+    key: Hmac<Sha256>,
+}
+
+impl Auth {
+    pub fn new(config: &Config) -> Auth {
+        Auth {
+            key: config.token_key.clone(),
+        }
+    }
+
+    pub async fn middleware<B>(
+        self,
+        req: Request<B>,
+        next: Next<B>,
+    ) -> Result<Response, StatusCode> {
+        let auth_header = req
+            .headers()
+            .get(http::header::AUTHORIZATION)
+            .and_then(|header| header.to_str().ok());
+
+        let token_string = header_to_token(auth_header);
+        if !self.verify_token(token_string) {
+            warn!("Unauthenticated REQUEST!!!")
+        }
+
+        // For now everything is authenticated
+        Ok(next.run(req).await)
+    }
+
+    fn verify_token(&self, token_string: Option<String>) -> bool {
+        if let Some(token) = token_string {
+            let claims: Result<serde_json::Value, _> = token.verify_with_key(&self.key);
+            claims.is_ok()
+        } else {
+            false
+        }
+    }
+}
+
+/// Tries to extract
+fn header_to_token(header: Option<&str>) -> Option<String> {
+    // The header is always the same now need to send that to the client.
+    const JWT_HEADER: &str = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.";
+
+    let header = header.and_then(|s| s.strip_prefix("Bearer "));
+
+    header.map(|token_body| format!("{}{}", JWT_HEADER, token_body))
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,16 @@
+mod auth;
 mod config;
 mod messaging;
 mod robot;
 mod server;
 
 use anyhow::Result;
+use axum::middleware;
 use axum::Router;
 use dotenvy::dotenv;
 use log::debug;
 
+use crate::auth::Auth;
 pub use crate::config::Config;
 
 #[tokio::main]
@@ -22,13 +25,19 @@ async fn main() -> Result<()> {
     let config = Config::load()?;
 
     debug!("Loading modules");
+    let auth = Auth::new(&config);
     let robot = robot::setup().await?;
     let messaging = messaging::setup().await?;
 
     debug!("Setting up routes");
+    let auth_sub = auth.clone();
     let app = Router::new()
         .nest("/api/robot", robot)
-        .nest("/api/messaging", messaging);
+        .nest("/api/messaging", messaging)
+        .route_layer(middleware::from_fn(move |req, next| {
+            let auth = auth_sub.clone();
+            auth.middleware(req, next)
+        }));
 
     debug!("Starting the server");
     server::serve(app, &config).await
