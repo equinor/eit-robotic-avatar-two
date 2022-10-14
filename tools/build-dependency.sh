9428ef3fc8bedd4bd5bcc603c1c1e9474052cbbb
Ole Martin Gjersvik <olgj@equinor.com>
2022-10-14 07:10:33 +0000

    Build needs its own script.

---
+++ tools/build-dependency.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+cd ~
+
+# Tooling for webasembely deveopment.
+rustup target add wasm32-unknown-unknown
+wget -qO- https://github.com/thedodd/trunk/releases/download/v0.16.0/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
+mv ./trunk /usr/local/bin/
