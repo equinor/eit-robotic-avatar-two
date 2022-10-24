f3c3c3b59073750e25a63786ae3e7d936719936f
Ole Martin Gjersvik <olgj@equinor.com>
2022-10-24 14:20:48 +0200

    Client authentication (#65)
    * Send token back to UI
    * Client Authentication

--- client/src/server.rs
+++ client/src/server.rs
@@ -2,23 +2,18 @@ use common::{RobotStatus, SendMessage};
 use gloo_net::http::Request;
 
 #[derive(Clone)]
-pub struct Server {}
+pub struct Server {
+    header: String,
+}
 
 impl Server {
-    pub fn new() -> Server {
-        Server {}
+    pub fn new(token: &str) -> Server {
+        Server {
+            header: format!("Bearer {}", token),
+        }
     }
 
-    pub async fn post_message(&self, msg: &SendMessage) {
-        Request::post("/api/messaging")
-            .json(&msg)
-            .unwrap()
-            .send()
-            .await
-            .unwrap();
-    }
-
-    pub async fn get_auth_login(&self) -> String {
+    pub async fn get_auth_login() -> String {
         Request::get("/api/auth/login")
             .send()
             .await
@@ -28,8 +23,18 @@ impl Server {
             .unwrap()
     }
 
+    pub async fn post_message(&self, msg: &SendMessage) {
+        self.post("/api/messaging")
+            .header("Authorization", &self.header)
+            .json(&msg)
+            .unwrap()
+            .send()
+            .await
+            .unwrap();
+    }
+
     pub async fn get_robot(&self) -> RobotStatus {
-        Request::get("/api/robot")
+        self.get("/api/robot")
             .send()
             .await
             .unwrap()
@@ -37,10 +42,12 @@ impl Server {
             .await
             .unwrap()
     }
-}
 
-impl Default for Server {
-    fn default() -> Self {
-        Self::new()
+    fn get(&self, url: &str) -> Request {
+        Request::get(url).header("Authorization", &self.header)
+    }
+
+    fn post(&self, url: &str) -> Request {
+        Request::post(url).header("Authorization", &self.header)
     }
 }
