b2b144ac13f4c70d764da981ccb9238946fadf44
Ole Martin Gjersvik <olgj@equinor.com>
2022-10-26 12:55:58 +0000

    Updated viewport so thats is easy to port over.

--- client/js/view/Viewport.mjs
+++ client/js/view/Viewport.mjs
@@ -27,59 +27,8 @@ export default class Viewport extends React.Component {
     }
 
     componentDidMount() {
-        // Based on the assumption both Oculus Quest 2 and Webcam have a diagonal field of view of 90. But a different aspect ratio.
-        let fov = 0.830097;
         this.componentDidUpdate();
-        const scene = new THREE.Scene();
-        const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1);
-        const renderer = new THREE.WebGLRenderer({
-            canvas: this.canvas.current
-        });
-
-        renderer.setSize(3664, 1920, false);
-        renderer.xr.enabled = true;
-        renderer.xr.cameraAutoUpdate = false;
-        // @ts-ignore
-        renderer.xr.getCamera = function () {
-            return camera;
-        };
-
-        const textureLeft = new THREE.VideoTexture(this.left.current);
-        textureLeft.center = new THREE.Vector2(0.5, 0.5);
-        textureLeft.rotation = 90 * 3 * (Math.PI / 180);
-        const left = new THREE.Mesh(new THREE.PlaneGeometry(0.58952 * fov, 2 * fov), new THREE.MeshBasicMaterial({ map: textureLeft }));
-        left.position.x = -0.5;
-        scene.add(left);
-        const textureRight = new THREE.VideoTexture(this.right.current);
-        textureRight.center = new THREE.Vector2(0.5, 0.5);
-        textureRight.rotation = 90 * 1 * (Math.PI / 180);
-        const right = new THREE.Mesh(new THREE.PlaneGeometry(0.58952 * fov, 2 * fov), new THREE.MeshBasicMaterial({ map: textureRight }));
-        right.position.x = 0.5;
-        scene.add(right);
-
-        renderer.setAnimationLoop((_, xrframe) => {
-            if (renderer.xr.isPresenting && this.props.onTrack) {
-                const r = xrframe.getViewerPose(renderer.xr.getReferenceSpace()).transform.orientation;
-                let track = {
-                    rx: r.x,
-                    ry: r.y,
-                    rz: r.z,
-                    l: { x: 0, y: 0, a: false, b: false, c: 0, d: 0 },
-                    r: { x: 0, y: 0, a: false, b: false, c: 0, d: 0 },
-                };
-                xrframe.session.inputSources.forEach(function (input) {
-                    if (input.handedness === "left" && input.gamepad) {
-                        track.l = toController(input.gamepad);
-                    }
-                    else if (input.handedness === "right" && input.gamepad) {
-                        track.r = toController(input.gamepad);
-                    }
-                });
-                this.props.onTrack(track);
-            }
-            renderer.render(scene, camera);
-        });
-        document.body.appendChild(VRButton.createButton(renderer));
+        setup_3d(this.canvas.current, this.left.current, this.right.current, this.props.onTrack);
     }
 
     componentDidUpdate() {
@@ -100,4 +49,60 @@ function toController(game) {
         c: (_h = (_g = game.buttons[0]) === null || _g === void 0 ? void 0 : _g.value) !== null && _h !== void 0 ? _h : 0,
         d: (_k = (_j = game.buttons[1]) === null || _j === void 0 ? void 0 : _j.value) !== null && _k !== void 0 ? _k : 0,
     };
+}
+
+export function setup_3d(canvas, left_video, right_video, onTrack) {
+    // Based on the assumption both Oculus Quest 2 and Webcam have a diagonal field of view of 90. But a different aspect ratio.
+    let fov = 0.830097;
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1);
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas
+    });
+
+    renderer.setSize(3664, 1920, false);
+    renderer.xr.enabled = true;
+    renderer.xr.cameraAutoUpdate = false;
+    // @ts-ignore
+    renderer.xr.getCamera = function () {
+        return camera;
+    };
+
+    const textureLeft = new THREE.VideoTexture(left_video);
+    textureLeft.center = new THREE.Vector2(0.5, 0.5);
+    textureLeft.rotation = 90 * 3 * (Math.PI / 180);
+    const left = new THREE.Mesh(new THREE.PlaneGeometry(0.58952 * fov, 2 * fov), new THREE.MeshBasicMaterial({ map: textureLeft }));
+    left.position.x = -0.5;
+    scene.add(left);
+    const textureRight = new THREE.VideoTexture(right_video);
+    textureRight.center = new THREE.Vector2(0.5, 0.5);
+    textureRight.rotation = 90 * 1 * (Math.PI / 180);
+    const right = new THREE.Mesh(new THREE.PlaneGeometry(0.58952 * fov, 2 * fov), new THREE.MeshBasicMaterial({ map: textureRight }));
+    right.position.x = 0.5;
+    scene.add(right);
+
+    renderer.setAnimationLoop((_, xrframe) => {
+        if (renderer.xr.isPresenting && onTrack) {
+            const r = xrframe.getViewerPose(renderer.xr.getReferenceSpace()).transform.orientation;
+            let track = {
+                rx: r.x,
+                ry: r.y,
+                rz: r.z,
+                l: { x: 0, y: 0, a: false, b: false, c: 0, d: 0 },
+                r: { x: 0, y: 0, a: false, b: false, c: 0, d: 0 },
+            };
+            xrframe.session.inputSources.forEach(function (input) {
+                if (input.handedness === "left" && input.gamepad) {
+                    track.l = toController(input.gamepad);
+                }
+                else if (input.handedness === "right" && input.gamepad) {
+                    track.r = toController(input.gamepad);
+                }
+            });
+            onTrack(track);
+        }
+        renderer.render(scene, camera);
+    });
+    document.body.appendChild(VRButton.createButton(renderer));
 }
