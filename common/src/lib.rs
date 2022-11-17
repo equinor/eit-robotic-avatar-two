29645748079462fb9e18cab4f8df6d5ac311cef5
Ole Martin Gjersvik <olgj@equinor.com>
2022-11-17 11:12:53 +0000

    No motors on connection loss

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -47,20 +47,20 @@ pub struct RobotStatus {
 
 // Legacy types from the old robotic avatar
 
-#[derive(Debug, Default, Clone, Copy, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, Copy, Serialize, Deserialize, PartialEq)]
 pub struct Tracking {
     pub head: Head,
     pub drive: Drive,
 }
 
-#[derive(Debug, Default, Clone, Copy, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, Copy, Serialize, Deserialize, PartialEq)]
 pub struct Head {
     pub rx: f64,
     pub ry: f64,
     pub rz: f64,
 }
 
-#[derive(Debug, Default, Clone, Copy, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, Copy, Serialize, Deserialize, PartialEq)]
 pub struct Drive {
     pub speed: f64,
     pub turn: f64,
--- server/src/services/minion.rs
+++ server/src/services/minion.rs
@@ -1,31 +1,47 @@
+use std::time::Duration;
+
 use common::{Drive, Head, RtcIce};
 use parking_lot::Mutex;
+use time::Instant;
 
-use crate::Config;
+const TIMEOUT: Duration = Duration::from_millis(500);
 
 pub struct Minion {
-    movement: Mutex<(Head, Drive)>,
+    movement: Mutex<Movement>,
     offer: Mutex<String>,
     answer: Mutex<String>,
     ice: RtcIce,
 }
 
 impl Minion {
-    pub fn new(config: &Config) -> Self {
+    pub fn new(ice: RtcIce) -> Self {
         Self {
-            movement: Default::default(),
+            movement: Mutex::new(Movement {
+                head: Head::default(),
+                drive: Drive::default(),
+                age: Instant::now(),
+            }),
             offer: Default::default(),
             answer: Default::default(),
-            ice: config.ice.clone(),
+            ice,
         }
     }
 
     pub fn movement(&self) -> (Head, Drive) {
-        *self.movement.lock()
+        let movement = self.movement.lock();
+        if movement.age.elapsed() < TIMEOUT {
+            (movement.head, movement.drive)
+        } else {
+            (movement.head, Drive::default())
+        }
     }
 
     pub fn movement_set(&self, head: Head, drive: Drive) {
-        *self.movement.lock() = (head, drive)
+        *self.movement.lock() = Movement {
+            head,
+            drive,
+            age: Instant::now(),
+        }
     }
 
     pub fn offer(&self) -> String {
@@ -62,5 +78,55 @@ impl Minion {
 
     pub fn ice(&self) -> RtcIce {
         self.ice.clone()
+    }
+}
+
+struct Movement {
+    head: Head,
+    drive: Drive,
+    age: Instant,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{thread, time::Duration};
+
+    use super::*;
+
+    #[test]
+    fn movement() {
+        let minion = Minion::new(RtcIce::default());
+
+        let test_data = (
+            Head {
+                rx: 0.1,
+                ry: 0.2,
+                rz: 0.3,
+            },
+            Drive {
+                speed: 1.0,
+                turn: 1.0,
+            },
+        );
+        minion.movement_set(test_data.0, test_data.1);
+
+        assert_eq!(minion.movement(), test_data);
+    }
+
+    #[test]
+    fn movement_timeout() {
+        let minion = Minion::new(RtcIce::default());
+
+        minion.movement_set(
+            Head::default(),
+            Drive {
+                speed: 1.0,
+                turn: 1.0,
+            },
+        );
+
+        thread::sleep(Duration::from_secs(1));
+
+        assert_eq!(minion.movement(), Default::default());
     }
 }
