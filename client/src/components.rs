616d769822a8c0acbac13f4e6222fa0db679c2f4
Ole Martin Gjersvik <olgj@equinor.com>
2022-11-20 17:50:04 +0000

    Start video button

--- client/src/components.rs
+++ client/src/components.rs
@@ -1,7 +1,8 @@
 use wasm_bindgen_futures::spawn_local;
+use web_sys::MediaStream;
 use yew::prelude::*;
 
-use crate::services::server;
+use crate::services::{server, webrtc};
 
 #[function_component(GenPin)]
 pub fn gen_pin() -> Html {
@@ -19,4 +20,28 @@ pub fn gen_pin() -> Html {
         Some(s) => html! {<span class={"pin"}>{s}</span>},
         None => html! {<button {onclick}>{"Generate login Pin"}</button>},
     }
+}
+
+#[derive(Properties, PartialEq)]
+pub struct HeadsetStreamProps {
+    pub callback: Callback<(MediaStream, MediaStream)>,
+}
+
+#[function_component(HeadsetStream)]
+pub fn headset_stream(props: &HeadsetStreamProps) -> Html {
+    let started = use_state(|| false);
+
+    let onclick = {
+        let started = started.clone();
+        let callback = props.callback.clone();
+        Callback::from(move |_| {
+            started.set(true);
+            let callback = callback.clone();
+            spawn_local(async move {
+                callback.emit(webrtc::receive().await);
+            })
+        })
+    };
+
+    html! {<button disabled={*started} {onclick}>{"Start video link with minion robot"}</button>}
 }
--- client/src/robotic.rs
+++ client/src/robotic.rs
@@ -4,43 +4,59 @@ mod robot;
 pub use self::minion::Minion;
 pub use self::robot::Robot;
 
+use web_sys::MediaStream;
 use yew::prelude::*;
 
-use crate::components::GenPin;
+use crate::components::{GenPin, HeadsetStream};
 
 #[derive(PartialEq, Eq, Properties)]
 pub struct Props {}
 
-pub struct Robotic {}
+pub enum Msg {
+    SetStreams((MediaStream, MediaStream)),
+}
+
+pub struct Robotic {
+    streams: Option<(MediaStream, MediaStream)>,
+}
 
 impl Component for Robotic {
-    type Message = ();
+    type Message = Msg;
     type Properties = Props;
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Robotic {}
+        Robotic { streams: None }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        false
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::SetStreams(s) => self.streams = Some(s),
+        };
+        true
     }
 
     #[allow(clippy::let_unit_value)]
-    fn view(&self, _ctx: &Context<Self>) -> Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+
         html! {
             <div class={"robotic"}>
                 <h1 class="header">{"Robotic Avatar"}</h1>
                 <content>
                     <ol>
                         <li>{"Generate pin for Meta Quest 2 headset: "} <GenPin/> </li>
-                        <li>{"Start video link with minion robot"}</li>
+                        <li><HeadsetStream callback={link.callback(Msg::SetStreams)}/></li>
                         <li>{"When you see the video click the ENTER VR button at the bottom of the screen"}</li>
                     </ol>
 
-
+                    {
+                        match &self.streams {
+                            Some(streams) => html!(<Minion left={streams.0.clone()} right={streams.1.clone()}/>),
+                            None =>  html!(<Minion/>)
+                        }
+                    }
 
                     <Robot/>
-                    <Minion/>
                 </content>
             </div>
         }
