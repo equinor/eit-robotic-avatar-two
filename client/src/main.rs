e14b05cf9e6414bd7b4dc6243ac8c7a29cd4310b
Ole Martin Gjersvik <olgj@equinor.com>
2022-11-20 16:07:51 +0000

    Moved ui into main.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,10 +1,50 @@
 mod agents;
 mod auth;
+mod login;
+mod robotic;
 mod services;
-mod ui;
 
-pub use ui::Ui;
+use yew::prelude::*;
+
+use login::Login;
+use robotic::Robotic;
+
+pub struct App {
+    logged_in: bool,
+}
+
+pub enum Msg {
+    Login,
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        App { logged_in: false }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Login => {
+                self.logged_in = true;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+
+        if self.logged_in {
+            html! (<Robotic/>)
+        } else {
+            html!(<Login on_login={link.callback(|_|Msg::Login)}/>)
+        }
+    }
+}
 
 fn main() {
-    yew::start_app::<Ui>();
+    yew::start_app::<App>();
 }
