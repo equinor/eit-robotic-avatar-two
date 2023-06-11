4cff846cd57b9dd60f784de1120b6f30d440e768
Ole Martin Gjersvik <olgj@equinor.com>
2023-06-11 15:22:53 +0200

    Cleaned up Headset code.

--- client/src/headset.rs
+++ client/src/headset.rs
@@ -3,78 +3,68 @@ use web_sys::{window, HtmlCanvasElement, HtmlElement, HtmlVideoElement, MediaStr
 
 use crate::services::tracking::Track;
 
-pub struct Wrapper {
+pub struct Headset {
     left: MediaStream,
     right: MediaStream,
 }
 
-impl Wrapper {
-    pub fn new() -> Wrapper {
-        Wrapper {
+impl Headset {
+    pub fn new() -> Headset {
+        Headset {
             left: MediaStream::new().unwrap(),
             right: MediaStream::new().unwrap(),
         }
     }
 
-    pub fn set_streams(&mut self, streams: &Option<(MediaStream, MediaStream)>) {
+    pub fn set_streams(&mut self, (new_left, new_right): (MediaStream, MediaStream)) {
         for track in self.left.get_tracks().iter() {
             self.left.remove_track(track.dyn_ref().unwrap())
         }
         for track in self.right.get_tracks().iter() {
             self.right.remove_track(track.dyn_ref().unwrap())
         }
-        if let Some((new_left, new_right)) = streams {
-            for track in new_left.get_tracks().iter() {
-                self.left.add_track(track.dyn_ref().unwrap())
-            }
-            for track in new_right.get_tracks().iter() {
-                self.right.add_track(track.dyn_ref().unwrap())
-            }
+        for track in new_left.get_tracks().iter() {
+            self.left.add_track(track.dyn_ref().unwrap())
+        }
+        for track in new_right.get_tracks().iter() {
+            self.right.add_track(track.dyn_ref().unwrap())
         }
     }
 
-    pub fn left_viewport(&self) -> Option<&MediaStream> {
-        Some(&self.left)
+    pub fn render(&self, root: &HtmlElement) {
+        root.set_class_name("viewport");
+
+        let document = window().unwrap().document().unwrap();
+        let canvas: HtmlCanvasElement = document
+            .create_element("canvas")
+            .unwrap()
+            .dyn_into()
+            .unwrap();
+        root.append_child(&canvas).unwrap();
+
+        let left: HtmlVideoElement = document
+            .create_element("video")
+            .unwrap()
+            .dyn_into()
+            .unwrap();
+        left.set_autoplay(true);
+        left.set_src_object(Some(&self.left));
+        root.append_child(&left).unwrap();
+
+        let right: HtmlVideoElement = document
+            .create_element("video")
+            .unwrap()
+            .dyn_into()
+            .unwrap();
+        right.set_autoplay(true);
+        right.set_src_object(Some(&self.right));
+        root.append_child(&right).unwrap();
+
+        let track = Track::default();
+        let closure = Closure::new(move |value| track.send(value));
+        setup_3d(&canvas, &left, &right, &closure);
+        closure.forget();
     }
-
-    pub fn right_viewport(&self) -> Option<&MediaStream> {
-        Some(&self.right)
-    }
-}
-
-pub fn headset(root: &HtmlElement, wrapper: &Wrapper) {
-    root.set_class_name("viewport");
-
-    let document = window().unwrap().document().unwrap();
-    let canvas: HtmlCanvasElement = document
-        .create_element("canvas")
-        .unwrap()
-        .dyn_into()
-        .unwrap();
-    root.append_child(&canvas).unwrap();
-
-    let left: HtmlVideoElement = document
-        .create_element("video")
-        .unwrap()
-        .dyn_into()
-        .unwrap();
-    left.set_autoplay(true);
-    left.set_src_object(wrapper.left_viewport());
-    root.append_child(&left).unwrap();
-
-    let right: HtmlVideoElement = document
-        .create_element("video")
-        .unwrap()
-        .dyn_into()
-        .unwrap();
-    right.set_autoplay(true);
-    right.set_src_object(wrapper.right_viewport());
-    root.append_child(&right).unwrap();
-
-    let track = Track::default();
-    let closure = Closure::new(move |value| track.send(value));
-    setup_3d(&canvas, &left, &right, &closure);
-    closure.forget();
 }
 
 #[wasm_bindgen(raw_module = "/js/viewport.mjs")]
