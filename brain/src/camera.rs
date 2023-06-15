f0e232a1316852fe3896a07cfc97eba767ba1c47
Ole Martin Gjersvik <olgj@equinor.com>
2023-06-15 07:45:07 +0200

    Base types for cameras. (#127)
    Defined the interface for a camera feed into the robot brain. Its the
    running environments job to create the Camera type.

---
+++ brain/src/camera.rs
@@ -0,0 +1,13 @@
+use std::sync::Arc;
+
+use image::RgbImage;
+use time::OffsetDateTime;
+use tokio::sync::watch::Receiver;
+
+pub type Camera = Receiver<Picture>;
+
+pub struct Picture {
+    pub seq: u64,
+    pub timestamp: OffsetDateTime,
+    pub image: Arc<RgbImage>,
+}
