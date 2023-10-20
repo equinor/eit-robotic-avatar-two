2a4effd97cede8b94fa9fb8091bd58398082e9ce
Ole Martin Gjersvik <olgj@equinor.com>
2023-10-20 15:43:36 +0200

    Websocket message on picture.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-use std::{sync::Arc, thread, time::Instant};
+use std::{sync::Arc, thread};
 
+use axum::{
+    extract::{
+        ws::{Message, WebSocket},
+        WebSocketUpgrade,
+    },
+    response::{Html, Response},
+    routing::get,
+    Router,
+};
 use nokhwa::{
     pixel_format::RgbFormat,
     utils::{CameraIndex, FrameFormat, RequestedFormat, RequestedFormatType, Resolution},
@@ -9,15 +18,12 @@ use tokio::sync::watch;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let mut eyes = eyes();
+    let eyes = eyes();
 
-    loop {
-        let start = Instant::now();
-        eyes.changed().await.unwrap();
-        let _frames = eyes.borrow_and_update().clone();
-        println!("Frame time: {:?}", start.elapsed());
-    }
+    transport(eyes).await;
 }
+
+type Sight = watch::Receiver<Arc<(Buffer, Buffer)>>;
 
 fn eyes() -> watch::Receiver<Arc<(Buffer, Buffer)>> {
     let null_buffer = Buffer::new(
@@ -50,4 +56,40 @@ fn eyes() -> watch::Receiver<Arc<(Buffer, Buffer)>> {
     });
 
     receiver
+}
+
+async fn transport(sight: Sight) {
+    let app = Router::new().route("/", get(index)).route(
+        "/ws",
+        get({
+            let sight_clone = sight.clone();
+            move |ws| upgrade(ws, sight_clone)
+        }),
+    );
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn index() -> Html<&'static str> {
+    Html(include_str!("./index.html"))
+}
+
+async fn upgrade(ws: WebSocketUpgrade, sight: Sight) -> Response {
+    ws.on_upgrade(|socket| websocket(socket, sight))
+}
+
+async fn websocket(mut socket: WebSocket, mut sight: Sight) {
+    while sight.changed().await.is_ok() {
+        if socket
+            .send(Message::Text("Hello from server!".to_string()))
+            .await
+            .is_err()
+        {
+            break;
+        }
+    }
 }
