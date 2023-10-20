f65ffe7e03a17ae8c85ea0c251f2cc0b26536951
Ole Martin Gjersvik <olgj@equinor.com>
2023-10-20 16:27:19 +0200

    Web socket transport (#142)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
-use std::{sync::Arc, thread, time::Instant};
+use std::{sync::Arc, thread};
 
+use axum::{
+    extract::{
+        ws::{Message, WebSocket},
+        WebSocketUpgrade,
+    },
+    response::{Html, Response},
+    routing::get,
+    Router,
+};
 use nokhwa::{
     pixel_format::RgbFormat,
     utils::{CameraIndex, FrameFormat, RequestedFormat, RequestedFormatType, Resolution},
     Buffer, Camera,
 };
+use serde::Serialize;
 use tokio::sync::watch;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let mut eyes = eyes();
+    let eyes = eyes();
 
-    loop {
-        let start = Instant::now();
-        eyes.changed().await.unwrap();
-        let _frames = eyes.borrow_and_update().clone();
-        println!("Frame time: {:?}", start.elapsed());
-    }
+    transport(eyes).await;
 }
+
+type Sight = watch::Receiver<Arc<(Buffer, Buffer)>>;
 
 fn eyes() -> watch::Receiver<Arc<(Buffer, Buffer)>> {
     let null_buffer = Buffer::new(
@@ -50,4 +57,47 @@ fn eyes() -> watch::Receiver<Arc<(Buffer, Buffer)>> {
     });
 
     receiver
+}
+
+async fn transport(sight: Sight) {
+    let app = Router::new().route("/", get(index)).route(
+        "/ws",
+        get({
+            let sight_clone = sight.clone();
+            move |ws| upgrade(ws, sight_clone)
+        }),
+    );
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn index() -> Html<&'static str> {
+    Html(include_str!("./index.html"))
+}
+
+async fn upgrade(ws: WebSocketUpgrade, sight: Sight) -> Response {
+    ws.on_upgrade(|socket| websocket(socket, sight))
+}
+
+#[derive(Serialize)]
+struct SendPacket<'a> {
+    pub eyes: (&'a [u8], &'a [u8]),
+}
+
+async fn websocket(mut socket: WebSocket, mut sight: Sight) {
+    while sight.changed().await.is_ok() {
+        let eyes = sight.borrow_and_update().clone();
+        let packet = SendPacket {
+            eyes: (eyes.0.buffer(), eyes.1.buffer()),
+        };
+        let msg = Message::Binary(rmp_serde::to_vec(&packet).unwrap());
+
+        if socket.send(msg).await.is_err() {
+            break;
+        }
+    }
 }
