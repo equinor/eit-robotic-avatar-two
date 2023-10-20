dd72fc9f5e7d31c370643ed650e3f556e746345b
Ole Martin Gjersvik <olgj@equinor.com>
2023-10-20 14:42:23 +0200

    Cameras as eyes. (#141)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-fn main() {
-    println!("A new hope");
+use std::{sync::Arc, thread, time::Instant};
+
+use nokhwa::{
+    pixel_format::RgbFormat,
+    utils::{CameraIndex, FrameFormat, RequestedFormat, RequestedFormatType, Resolution},
+    Buffer, Camera,
+};
+use tokio::sync::watch;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let mut eyes = eyes();
+
+    loop {
+        let start = Instant::now();
+        eyes.changed().await.unwrap();
+        let _frames = eyes.borrow_and_update().clone();
+        println!("Frame time: {:?}", start.elapsed());
+    }
+}
+
+fn eyes() -> watch::Receiver<Arc<(Buffer, Buffer)>> {
+    let null_buffer = Buffer::new(
+        Resolution {
+            width_x: 0,
+            height_y: 0,
+        },
+        &[],
+        FrameFormat::RAWRGB,
+    );
+
+    let (sender, receiver) = watch::channel(Arc::new((null_buffer.clone(), null_buffer)));
+
+    thread::spawn(move || {
+        let mut camera_a = Camera::new(
+            CameraIndex::Index(0),
+            RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestResolution),
+        )
+        .unwrap();
+        let mut camera_b = Camera::new(
+            CameraIndex::Index(1),
+            RequestedFormat::new::<RgbFormat>(RequestedFormatType::AbsoluteHighestResolution),
+        )
+        .unwrap();
+        loop {
+            let a = camera_a.frame().unwrap();
+            let b = camera_b.frame().unwrap();
+            sender.send(Arc::new((a, b))).unwrap();
+        }
+    });
+
+    receiver
 }
