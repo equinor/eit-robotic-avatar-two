3117aa5c97894915a805337b0f1179819db08808
Ole Martin Gjersvik <olgj@equinor.com>
2023-10-20 10:54:02 +0200

    Create new main create moved the old to minion. (#140)

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,3 @@
-mod config;
-mod server;
-mod tracking;
-
-use avatar::{arduino, controller, ros, ControllerInputs, ControllerOutputs};
-use config::Config;
-use server::Server;
-use tokio::signal;
-
-#[tokio::main]
-async fn main() {
-    start().await;
-    signal::ctrl_c().await.unwrap();
-}
-
-async fn start() {
-    let config = Config::get_config();
-    let server = Server::connect(config).await;
-
-    let tracking = tracking::tracking(server);
-
-    let ControllerOutputs {
-        drive: drive_receive,
-        arm: arm_receive,
-    } = controller(ControllerInputs { pilot: tracking });
-
-    arduino::register(drive_receive);
-    ros::register(arm_receive);
+fn main() {
+    println!("A new hope");
 }
